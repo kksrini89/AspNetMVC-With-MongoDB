900f163babe2d8ee0588213632cebb2296047efe
srinivasan <kksrini89@gmail.com>
2016-03-02 20:02:53 +0530

    Adding Values from ASPMVC To MongoDB

---
+++ SampleAspNetMVC-MongoDB/Controllers/CarsController.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using SampleAspNetMVC_MongoDB.Models;
+using SampleAspNetMVC_MongoDB.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleAspNetMVC_MongoDB.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IMongoClient Client;
+        public IMongoDatabase Database;
+        public CarsController()
+        {
+            Client = new MongoClient(Settings.Default.SampleConnectionString);
+            Database = Client.GetDatabase("sample");
+        }
+        // GET: Cars
+        public ActionResult Index()
+        {
+            var cars = Database.GetCollection<Car>("car");
+            List<Car> lists = new List<Car>();
+            using (var documentList = cars.Find(new BsonDocument()).ToCursor()) //Gets all documents
+            {
+                while (documentList.MoveNext())
+                {
+                    foreach (var item in documentList.Current)
+                    {
+                        lists.Add(item);
+                    }
+                }
+            }
+            return View(lists);
+        }
+
+        public ActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpPost]     
+        public ActionResult Post(Car car)
+        {
+            var collection = Database.GetCollection<Car>("car");
+            collection.InsertOne(car);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- SampleAspNetMVC-MongoDB/Models/Car.cs
+++ SampleAspNetMVC-MongoDB/Models/Car.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,9 +7,17 @@ using System.Web;
 
 namespace SampleAspNetMVC_MongoDB.Models
 {
+    //public class Car
+    //{
+    //    public ObjectId _id { get; set; }
+    //    public string name { get; set; }
+    //    public string year { get; set; }
+    //    public string origin { get; set; }
+    //}
     public class Car
     {
-        public ObjectId _id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
         public string name { get; set; }
         public string year { get; set; }
         public string origin { get; set; }
